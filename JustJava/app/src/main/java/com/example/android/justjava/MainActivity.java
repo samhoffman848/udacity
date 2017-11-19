cb83e8c828727c9b863c45a802b0479c12f5d32d
Sam <sam@touchsurgery.com>
2017-11-19 15:51:39 +0000

    final just java implementation

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -12,11 +12,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import java.text.NumberFormat;
 
 /**
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
+    int quantity = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,17 +27,60 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
+     * This method is called when the + button is clicked.
+     */
+    public void increment(View view) {
+        quantity++;
+        displayQuantity(quantity);
+        displayPrice(quantity * 5);
+    }
+
+    /**
+     * This method is called when the - button is clicked.
+     */
+    public void decrement(View view) {
+        quantity--;
+
+        if(quantity < 0) {
+          quantity = 0;
+        }
+
+        displayQuantity(quantity);
+        displayPrice(quantity * 5);
+    }
+
+    /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(1);
+        displayMessage(quantity * 5);
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+    private void displayQuantity(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_num);
         quantityTextView.setText(String.valueOf(number));
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_num);
+        String price = "Total: " + NumberFormat.getCurrencyInstance().format(number);
+
+        priceTextView.setText(price);
+    }
+
+    /**
+     * This method displays the given price on the screen and a thank you message.
+     */
+    private void displayMessage(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_num);
+        String price = "Total: " + NumberFormat.getCurrencyInstance().format(number) + "\nThank you for your purchase!";
+
+        priceTextView.setText(price);
     }
 }
