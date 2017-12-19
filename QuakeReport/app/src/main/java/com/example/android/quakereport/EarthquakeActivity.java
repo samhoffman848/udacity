793271cb3733191b1a979bd01bdd263ddc6bea48
Sam <sam@touchsurgery.com>
2017-12-19 21:36:37 +0000

    request real json data from web using loader plus some design improvements

--- QuakeReport/app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ QuakeReport/app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -15,42 +15,61 @@
  */
 package com.example.android.quakereport;
 
+import android.app.LoaderManager;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.Context;
 import android.content.Intent;
+import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class EarthquakeActivity extends AppCompatActivity {
+public class EarthquakeActivity extends AppCompatActivity implements LoaderCallbacks<List<QuakeItem>>{
+    /** URL to query the USGS dataset for earthquake information */
+    private static final String USGS_REQUEST_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=3&limit=25";
 
     public static final String LOG_TAG = EarthquakeActivity.class.getName();
+
+    private static final int EARTHQUAKE_LOADER_ID = 1;
+
+    private QuakeAdapter mAdapter;
+    private TextView mEmptyListTextView;
+    ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
 
-        // Create a fake list of earthquake locations.
-        final ArrayList<QuakeItem> earthquakes = QueryUtils.extractEarthquakes();
-
         // Create a new {@link ArrayAdapter} of earthquakes
-        QuakeAdapter adapter = new QuakeAdapter(this, earthquakes);
+        mAdapter = new QuakeAdapter(this, new ArrayList<QuakeItem>());
 
         // Find a reference to the {@link ListView} in the layout
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
 
         // Set the adapter on the {@link ListView}
         // so the list can be populated in the user interface
-        earthquakeListView.setAdapter(adapter);
+        earthquakeListView.setAdapter(mAdapter);
+
+        mEmptyListTextView = (TextView) findViewById(R.id.emptyListTextView);
+        earthquakeListView.setEmptyView(mEmptyListTextView);
+
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
 
         earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int pos, long l) {
-                QuakeItem quakeItem = earthquakes.get(pos);
+                QuakeItem quakeItem = mAdapter.getItem(pos);
 
                 Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(quakeItem.getUrl()));
                 if (intent.resolveActivity(getPackageManager()) != null) {
@@ -58,5 +77,57 @@ public class EarthquakeActivity extends AppCompatActivity {
                 }
             }
         });
+
+        ConnectivityManager connectivityManager = (ConnectivityManager)this.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null && activeNetwork.isConnected();
+
+        if (isConnected){
+            Bundle bundle = new Bundle();
+            bundle.putString("url", USGS_REQUEST_URL);
+
+            LoaderManager loaderManager = getLoaderManager();
+            getLoaderManager().initLoader(EARTHQUAKE_LOADER_ID, bundle, this);
+        } else {
+            mEmptyListTextView.setText(R.string.no_connection);
+            mProgressBar.setVisibility(View.GONE);
+        }
+    }
+
+    public void sendNewRequest(View view){
+        String testUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=2&limit=25";
+
+        Bundle bundle = new Bundle();
+        bundle.putString("url", testUrl);
+
+        getLoaderManager().restartLoader(EARTHQUAKE_LOADER_ID, bundle, this);
+    }
+
+    @Override
+    public Loader<List<QuakeItem>> onCreateLoader(int i, Bundle args) {
+        String url = args.getString("url");
+
+        // Create a new loader for the given URL
+        return new EarthquakeLoader(this, url);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<QuakeItem>> loader, List<QuakeItem> earthquakes) {
+        mAdapter.clear();
+
+        mProgressBar.setVisibility(View.GONE);
+
+        if (earthquakes != null && !earthquakes.isEmpty()){
+            mAdapter.addAll(earthquakes);
+            mEmptyListTextView.setText("");
+        } else {
+            mEmptyListTextView.setText(R.string.no_earthquake);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<QuakeItem>> loader) {
+        mAdapter.clear();
     }
 }
---
+++ QuakeReport/app/src/main/java/com/example/android/quakereport/Utils.java
@@ -0,0 +1,97 @@
+package com.example.android.quakereport;
+
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+public final class Utils {
+    public static final String LOG_TAG = Utils.class.getSimpleName();
+
+    // Return String of Earthquake GeoJSON from USGS Query
+    public static String getEarthquakeData(String requestUrl){
+        URL url = createUrl(requestUrl);
+
+        String jsonResponse = null;
+        try{
+            jsonResponse = makeHttpRequest(url);
+        } catch (IOException e){
+            Log.e(LOG_TAG, "IOException Error: Trying to get JSON request", e);
+        }
+
+        return jsonResponse;
+    }
+
+    // Return URL object from String URL
+    private static URL createUrl(String stringUrl){
+        URL url = null;
+
+        try {
+            url = new URL(stringUrl);
+        } catch (MalformedURLException e){
+            Log.e(LOG_TAG, "Malformed URL Error: Trying to create URL", e);
+        }
+
+        return url;
+    }
+
+    // Make HTTP GET request for JSON data
+    private static String makeHttpRequest(URL url) throws IOException{
+        String jsonResponse = "";
+
+        // Catch null url and return
+        if (url==null){
+            return jsonResponse;
+        }
+
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+
+        try{
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(10000); // Milliseconds
+            urlConnection.setConnectTimeout(15000); // Milliseconds
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            if (urlConnection.getResponseCode()==200){
+                inputStream = urlConnection.getInputStream();
+                jsonResponse = readFromStream(inputStream);
+            } else {
+                Log.e(LOG_TAG, "HTTP Request Failed, Response Code: " + urlConnection.getResponseCode());
+            }
+        } catch (IOException e){
+            Log.e(LOG_TAG, "IOException Error: Problem getting JSON data", e);
+        } finally {
+            if (urlConnection != null){
+                urlConnection.disconnect();
+            }
+            if (inputStream != null){
+                inputStream.close();
+            }
+        }
+
+        return jsonResponse;
+    }
+
+    private static String readFromStream(InputStream inputStream) throws IOException{
+        StringBuilder output = new StringBuilder();
+        if (inputStream != null){
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line != null){
+                output.append(line);
+                line = reader.readLine();
+            }
+        }
+
+        return output.toString();
+    }
+}
