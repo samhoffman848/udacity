6e5bb35606750dde030bf5b11e972872a8423321
Sam <sam@touchsurgery.com>
2018-04-22 16:58:02 +0100

    initial pets app

---
+++ Pets/app/src/main/java/com/example/android/pets/data/PetDbHelper.java
@@ -0,0 +1,27 @@
+package com.example.android.pets.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class PetDbHelper extends SQLiteOpenHelper {
+    // Database file name
+    private static final String DATABASE_NAME = "shelter.db";
+
+    // Database version. If you change the schema you must increment that db version.
+    private static final int DATABASE_VERSION = 1;
+
+    public PetDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
